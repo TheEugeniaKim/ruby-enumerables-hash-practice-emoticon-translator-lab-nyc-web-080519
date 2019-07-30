68d67443826eb1ab4e709c8f7c38b598adbf2eb4
Eugenia Kim <elk303@nyu.edu>
2019-07-30 14:41:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,50 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+  load_library = YAML.load_file(file_path)
+  
+  load_library = {
+    "get_meaning" => {}, "get_emoticon" => {}
+  }
+  
+  load_library.each do |key, value|
+    load_library["get_meaning"].push(load_library[1], key)
+    load_library["get_emoticon"].push(load_library[0], load_library[1])  
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
 
-def get_english_meaning
-  # code goes here
-end
+  load_library
+
+end   
+
+
+
+# def get_japanese_emoticon(file_path ="./lib/emoticons.yml", emoticon)
+#   library = load_library(file_path)
+    
+#     if library ["get_emoticon"].include?(value)
+#       library["get_emoticon"][value]
+#     else
+#       "Sorry, that emoticon was not found"
+#     end
+  
+# end
+
+
+
+# def get_english_meaningfile_path ='./lib/emoticons.yml', emoticon)
+#   library = load_library(file_path)
+  
+#   if library["get_emoticon"].include?(value)
+#     library["get_emoticon"][value]
+#   else
+#     "Sorry, that emoticon was not found"
+#   end
+ 
+# end
+
+
+
+
